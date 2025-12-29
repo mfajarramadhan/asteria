249e0ad02da389fd053755744d087e32e2152da7
mfajarramadhan <mfajarramadhan04@gmail.com>
2025-12-29 21:35:50 +0700

    Fix: Resolve error

--- database/migrations/2025_09_30_141648_create_form_kp_scissor_lifts_table.php
+++ database/migrations/2025_09_30_141648_create_form_kp_scissor_lifts_table.php
@@ -1,4 +1,4 @@
-<?php
+s<?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -67,11 +67,7 @@ return new class extends Migration
             $table->text('lifting2')->nullable();
             $table->text('hasil2')->nullable();
             $table->text('keterangan2')->nullable();
-<<<<<<< HEAD
             $table->text('radius_putaran_kiri')->nullable();
-=======
-            $table->text('radius_putaran_kiri', 25)->nullable();
->>>>>>> 94ce5bf8c09da675a11a1807e6003fe35ed757c6
 
             $table->text('swl_tinggi_angkat3')->nullable();
             $table->text('beban_uji_load_chard3')->nullable();
